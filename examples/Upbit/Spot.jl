280a63351b0a72e468e44556f2a4746162ab3d26
Lisa <e.gryumova@yandex.ru>
2024-03-25 18:13:34 +0300

    Add examples for Upbit

---
+++ examples/Upbit/Spot.jl
@@ -0,0 +1,20 @@
+# Upbit/Spot
+# https://docs.upbit.com
+
+using Dates
+using CryptoAPIs
+using CryptoAPIs.Upbit
+
+CryptoAPIs.Upbit.Spot.day_candle(;
+    market = "KRW-BTC",
+    convertingPriceUnit = "KRW",
+    count = 1,
+    to = DateTime("2023-01-01T00:00:00")
+)
+
+CryptoAPIs.Upbit.Spot.market_list(; isDetails = true)
+
+CryptoAPIs.Upbit.Spot.order_book(; markets = "KRW-BTC")
+
+CryptoAPIs.Upbit.Spot.ticker(; markets = "KRW-BTC")
+CryptoAPIs.Upbit.Spot.ticker(; markets = ["KRW-BTC", "BTC-ETH"])
