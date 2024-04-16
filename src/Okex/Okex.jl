a9b9bb8c530620d0b3b000d5bdf83300fcf1775b
dgerasko <dgerasko@tunatech.io>
2024-04-16 17:49:10 +0300

    Add Okex exchange

---
+++ src/Okex/Okex.jl
@@ -0,0 +1,141 @@
+module Okex
+
+export OkexCommonQuery,
+    OkexPublicQuery,
+    OkexAccessQuery,
+    OkexPrivateQuery,
+    OkexAPIError,
+    OkexClient,
+    OkexData
+
+using Serde
+using Dates, NanoDates, TimeZones, Base64, Nettle
+
+using ..CryptoAPIs
+import ..CryptoAPIs: Maybe, AbstractAPIsError, AbstractAPIsData, AbstractAPIsQuery, AbstractAPIsClient
+
+abstract type OkexData <: AbstractAPIsData end
+abstract type OkexCommonQuery  <: AbstractAPIsQuery end
+abstract type OkexPublicQuery  <: OkexCommonQuery end
+abstract type OkexAccessQuery  <: OkexCommonQuery end
+abstract type OkexPrivateQuery <: OkexCommonQuery end
+
+"""
+    Data{D} <: AbstractAPIsData
+
+## Required fields
+- `msg::String`: Return message.
+- `code::Int64`: Return code.
+- `data::Vector{D}`: Result values.
+"""
+struct Data{D<:AbstractAPIsData} <: AbstractAPIsData
+    msg::String
+    code::Int64
+    data::Vector{D}
+
+    function Data{D}(msg::String, code::Int64, data::Vector{D}) where {D <: AbstractAPIsData}
+        @assert code == 0
+        return new{D}(msg, code, data)
+    end
+end
+
+"""
+    OkexClient <: AbstractAPIsClient
+
+Client info.
+
+## Required fields
+- `base_url::String`: Base URL for the client.
+
+## Optional fields
+- `public_key::String`: Public key for authentication.
+- `secret_key::String`: Secret key for authentication.
+- `interface::String`: Interface for the client.
+- `proxy::String`: Proxy information for the client.
+- `account_name::String`: Account name associated with the client.
+- `description::String`: Description of the client.
+"""
+Base.@kwdef struct OkexClient <: AbstractAPIsClient
+    base_url::String
+    public_key::Maybe{String} = nothing
+    secret_key::Maybe{String} = nothing
+    passphrase::Maybe{String} = nothing
+    interface::Maybe{String} = nothing
+    proxy::Maybe{String} = nothing
+    account_name::Maybe{String} = nothing
+    description::Maybe{String} = nothing
+end
+
+"""
+    OkexAPIError{T} <: AbstractAPIsError
+
+Exception thrown when an API method fails with code `T`.
+
+## Required fields
+- `code::Int64`: Error code.
+- `msg::String`: Error message.
+
+## Optional fields
+- `data::Vector{Any}`: Error result data.
+"""
+struct OkexAPIError{T} <: AbstractAPIsError
+    code::Int64
+    msg::String
+    data::Maybe{Vector{Any}}
+
+    function OkexAPIError(code::Int64, x...)
+        return new{code}(code, x...)
+    end
+end
+
+CryptoAPIs.error_type(::OkexClient) = OkexAPIError
+
+function Base.show(io::IO, e::OkexAPIError)
+    return print(io, "code = ", "\"", e.code, "\"", ", ", "msg = ", "\"", e.msg, "\"")
+end
+
+function CryptoAPIs.request_sign!(::OkexClient, query::Q, ::String)::Q where {Q<:OkexPublicQuery}
+    return query
+end
+
+function CryptoAPIs.request_sign!(client::OkexClient, query::Q, endpoint::String)::Q where {Q<:OkexPrivateQuery}
+    query.signature = nothing
+    str_query = Serde.to_query(query)
+    body::String = isempty(str_query) ? "" : "?" * str_query
+    query.timestamp = Dates.now(UTC)
+    salt = string(Dates.format(query.timestamp, "yyyy-mm-ddTHH:MM:SS.sss\\Z"), "GET", "/", endpoint, body)
+    query.signature = Base64.base64encode(digest("sha256", client.secret_key, salt))
+    return query
+end
+
+function CryptoAPIs.request_body(::Q)::String where {Q<:OkexCommonQuery}
+    return ""
+end
+
+function CryptoAPIs.request_query(query::Q)::String where {Q<:OkexCommonQuery}
+    return Serde.to_query(query)
+end
+
+function CryptoAPIs.request_headers(client::OkexClient, ::OkexPublicQuery)::Vector{Pair{String,String}}
+    return Pair{String,String}[
+        "Content-Type" => "application/json"
+    ]
+end
+
+function CryptoAPIs.request_headers(client::OkexClient, query::OkexPrivateQuery)::Vector{Pair{String,String}}
+    return Pair{String,String}[
+        "OK-ACCESS-TIMESTAMP" => Dates.format(query.timestamp, "yyyy-mm-ddTHH:MM:SS.sss\\Z"),
+        "Content-Type" => "application/json",
+        "OK-ACCESS-KEY" => client.public_key,
+        "OK-ACCESS-SIGN" => query.signature,
+        "OK-ACCESS-PASSPHRASE" => client.passphrase,
+    ]
+end
+
+include("Utils.jl")
+include("Errors.jl")
+
+include("Spot/Spot.jl")
+using .Spot
+
+end
---
+++ src/Okex/Spot/API/Currency.jl
@@ -0,0 +1,128 @@
+module Currency
+
+export CurrencyQuery,
+    CurrencyData,
+    currency
+
+using Serde
+using Dates, NanoDates, TimeZones
+
+using CryptoAPIs.Okex
+using CryptoAPIs.Okex: Data
+using CryptoAPIs: Maybe, APIsRequest
+
+Base.@kwdef mutable struct CurrencyQuery <: OkexPrivateQuery
+    ccy::String = ""
+
+    signature::Maybe{String} = nothing
+    timestamp::Maybe{DateTime} = nothing
+end
+
+struct CurrencyData <: OkexData
+    canDep::Bool
+    canInternal::Bool
+    canWd::Bool
+    ccy::String
+    chain::String
+    depQuotaFixed::Maybe{Float64}
+    depQuoteDailyLayer2::Maybe{Int64}
+    logoLink::Maybe{String}
+    mainNet::Bool
+    maxFee::Float64
+    maxWd::Float64
+    minDep::Float64
+    minDepArrivalConfirm::Maybe{Int64}
+    minFee::Float64
+    minWd::Float64
+    minWdUnlockConfirm::Maybe{Int64}
+    name::Maybe{String}
+    needTag::Bool
+    usedDepQuotaFixed::Maybe{Float64}
+    usedWdQuota::Maybe{Int64}
+    wdQuota::Maybe{Int64}
+    wdTickSz::Maybe{Int64}
+end
+
+function Serde.isempty(::Type{<:CurrencyData}, x)::Bool
+    return x === ""
+end
+
+"""
+    currency(client::OkexClient, query::CurrencyQuery)
+    currency(client::OkexClient; kw...)
+
+Get information of coins (available for deposit and withdraw) for user.
+
+[`GET api/v5/asset/currencies`](https://www.okx.com/docs-v5/en/#rest-api-funding-get-currencies)
+
+## Parameters:
+
+| Parameter | Type     | Required | Description |
+|:----------|:---------|:---------|:------------|
+| ccy       | String   | false    |             |
+| signature | String   | false    |             |
+| timestamp | DateTime | false    |             |
+
+## Code samples:
+
+```julia
+using Serde
+using CryptoAPIs.Okex
+
+okex_client = OkexClient(;
+    base_url = "https://www.okex.com",
+    public_key = ENV["OKEX_PUBLIC_KEY"],
+    secret_key = ENV["OKEX_SECRET_KEY"],
+    passphrase = ENV["OKEX_PASSPHRASE"],
+)
+
+result = Okex.Spot.currency(okex_client; ccy = "BTC")
+
+to_pretty_json(result.result)
+```
+
+## Result:
+
+```json
+{
+  "msg":"",
+  "code":0,
+  "data":[
+    {
+      "canDep":true,
+      "canInternal":true,
+      "canWd":true,
+      "ccy":"BTC",
+      "chain":"BTC-Bitcoin",
+      "depQuotaFixed":null,
+      "depQuoteDailyLayer2":null,
+      "logoLink":"https://static.coinall.ltd/cdn/oksupport/asset/currency/icon/btc20230419112752.png",
+      "mainNet":true,
+      "maxFee":0.0006,
+      "maxWd":500.0,
+      "minDep":0.0005,
+      "minDepArrivalConfirm":1,
+      "minFee":0.0003,
+      "minWd":0.001,
+      "minWdUnlockConfirm":2,
+      "name":"Bitcoin",
+      "needTag":false,
+      "usedDepQuotaFixed":null,
+      "usedWdQuota":0,
+      "wdQuota":40000000,
+      "wdTickSz":8
+    },
+    ...
+  ]
+}
+```
+"""
+function currency(client::OkexClient, query::CurrencyQuery)
+    return APIsRequest{Data{CurrencyData}}("GET", "api/v5/asset/currencies", query)(client)
+end
+
+function currency(client::OkexClient; kw...)
+    return currency(client, CurrencyQuery(; kw...))
+end
+
+end
