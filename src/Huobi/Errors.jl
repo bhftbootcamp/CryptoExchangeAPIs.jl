64386d344e4688cacb665a0fb0eae7143aad7990
Artem Emelin <153018291+artememelin@users.noreply.github.com>
2024-05-16 18:47:05 +0300

    Formatting

--- src/Huobi/Errors.jl
+++ src/Huobi/Errors.jl
@@ -7,11 +7,6 @@ import ..CryptoAPIs: isretriable, retry_maxcount, retry_timeout
 # UNDEF
 isretriable(e::APIsResult{HuobiAPIError}) = true
 
-# INVALID_PARAMETER
-function isretriable(e::APIsResult{HuobiAPIError{Symbol("invalid-parameter")}})
-    return e.result.err_msg == "request limit" ? true : false
-end
-
 # GATEWAY_INTERNAL_ERROR
 isretriable(e::APIsResult{HuobiAPIError{Symbol("gateway-internal-error")}}) = true
 retry_maxcount(e::APIsResult{HuobiAPIError{Symbol("gateway-internal-error")}}) = 20
