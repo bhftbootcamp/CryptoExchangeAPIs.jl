16f4fabaa45aa1bd9a916d22639daff97d9b5fc2
eugenekartvelishvili <evgeniikartvelishvili@bhftbootcamp.com>
2024-07-06 14:25:49 +0300

    Updated ServerTime module

--- src/Binance/Spot/API/ServerTime.jl
+++ src/Binance/Spot/API/ServerTime.jl
@@ -1,6 +1,8 @@
 module ServerTime
 
-export ServerTimeQuery, ServerTimeData, server_time
+export ServerTimeQuery, 
+    ServerTimeData, 
+    server_time
 
 using Serde
 using Dates, NanoDates, TimeZones
@@ -8,21 +10,30 @@ using Dates, NanoDates, TimeZones
 using CryptoAPIs.Binance
 using CryptoAPIs: Maybe, APIsRequest
 
-struct ServerTimeQuery <: BinancePublicQuery end
+Base.@kwdef struct ServerTimeQuery <: BinancePublicQuery
+    #__ empty
+end
 
 struct ServerTimeData <: BinanceData
-    serverTime::Int64
+    serverTime::NanoDate
 end
 
 """
     server_time(client::BinanceClient)
-    server_time(client::BinanceClient = Binance.Spot.public_client)
+    server_time(client::BinanceClient = Binance.Spot.public_client; kw...)
 
 Check Server Time.
 
 [`GET api/v3/time`](https://binance-docs.github.io/apidocs/spot/en/#check-server-time)
 
 Test connectivity to the Rest API and get the current server time.
+
+## Parameters:
+- `client::BinanceClient`: The Binance client.
+- `kw...`: Additional keyword arguments for `ServerTimeQuery`.
+
+## Returns:
+- `APIsResult{ServerTimeData}`: The server time data.
 
 ## Code samples:
 
@@ -43,12 +54,12 @@ to_pretty_json(result.result)
 }
 ```
 """
-function server_time(client::BinanceClient)
-    return APIsRequest{ServerTimeData}("GET", "api/v3/time", ServerTimeQuery())(client)
+function server_time(client::BinanceClient, query::ServerTimeQuery)
+    return APIsRequest{ServerTimeData}("GET", "api/v3/time", query)(client)
 end
-    
-function server_time(; client::BinanceClient = Binance.Spot.public_client)
-    return server_time(client)
+
+function server_time(client::BinanceClient = Binance.Spot.public_client; kw...)
+    return server_time(client, ServerTimeQuery(; kw...))
 end
-    
+
 end
