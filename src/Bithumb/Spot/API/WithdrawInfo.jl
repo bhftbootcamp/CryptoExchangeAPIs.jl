ef564175aafa01029eb9a586d5f7a9d22e4c5412
Cors88 <epopov@bhft.com>
2025-01-31 15:05:28 +0300

    Update WithdrawInfo.jl

--- src/Bithumb/Spot/API/WithdrawInfo.jl
+++ src/Bithumb/Spot/API/WithdrawInfo.jl
@@ -1,9 +1,8 @@
 module WithdrawInfo
 
-Base.@kwdef mutable struct WithdrawInfoQuery <: PrivateQuery
+Base.@kwdef mutable struct WithdrawInfoQuery <: BithumbPrivateQuery
     currency::String
     net_type::String
-
     signature::Maybe{String} = nothing
 end
 
@@ -46,53 +45,26 @@ struct WithdrawLimit
     remaining_daily_krw::Maybe{Float64}
 end
 
-struct WithdrawInfo <: AbstractResult
+struct WithdrawInfo <: BithumbData
     member_level::MemberLevel
     currency::Currency
     account::Account
     withdraw_limit::WithdrawLimit
 end
 
-security_level(x::WithdrawInfo) = x.member_level.security_level
-fee_level(x::WithdrawInfo) = x.member_level.fee_level
-email_verified(x::WithdrawInfo) = x.member_level.email_verified
-identity_auth_verified(x::WithdrawInfo) = x.member_level.identity_auth_verified
-bank_account_verified(x::WithdrawInfo) = x.member_level.bank_account_verified
-two_factor_auth_verified(x::WithdrawInfo) = x.member_level.two_factor_auth_verified
-member_level_locked(x::WithdrawInfo) = x.member_level.locked
-wallet_locked(x::WithdrawInfo) = x.member_level.wallet_locked
-code(x::WithdrawInfo) = x.currency.code
-withdraw_fee(x::WithdrawInfo) = x.currency.withdraw_fee
-is_coin(x::WithdrawInfo) = x.currency.is_coin
-wallet_state(x::WithdrawInfo) = x.currency.wallet_state
-wallet_support(x::WithdrawInfo) = x.currency.wallet_support
-account_currency(x::WithdrawInfo) = x.account.currency
-balance(x::WithdrawInfo) = x.account.balance
-account_locked(x::WithdrawInfo) = x.account.locked
-avg_buy_price(x::WithdrawInfo) = x.account.avg_buy_price
-avg_buy_price_modified(x::WithdrawInfo) = x.account.avg_buy_price_modified
-unit_currency(x::WithdrawInfo) = x.account.unit_currency
-withdraw_limit_currency(x::WithdrawInfo) = x.withdraw_limit.currency
-minimum(x::WithdrawInfo) = x.withdraw_limit.minimum
-onetime(x::WithdrawInfo) = x.withdraw_limit.onetime
-daily(x::WithdrawInfo) = x.withdraw_limit.daily
-remaining_daily(x::WithdrawInfo) = x.withdraw_limit.remaining_daily
-fixed(x::WithdrawInfo) = x.withdraw_limit.fixed
-can_withdraw(x::WithdrawInfo) = x.withdraw_limit.can_withdraw
-remaining_daily_krw(x::WithdrawInfo) = x.withdraw_limit.remaining_daily_krw
-
-
 """
-    withdraw_info(client::Client; $(CCTX.query_info(WithdrawInfoQuery)))
+    withdraw_info(client::BithumbClient, query::WithdrawInfoQuery)
 Check the possible withdrawal information of the currency.
 https://apidocs.bithumb.com/reference/%EC%B6%9C%EA%B8%88-%EA%B0%80%EB%8A%A5-%EC%A0%95%EB%B3%B4
 """
-function withdraw_info(client::Client, query::WithdrawInfoQuery)
-    return CctxQuery{WithdrawInfo}("GET", "v1/withdraws/chance", query)(client)
+function withdraw_info(client::BithumbClient, query::WithdrawInfoQuery)
+    CryptoExchangeAPIs.request_sign!(client, query, "v1/withdraws/chance")
+    
+    return APIsRequest{Data{WithdrawInfo}}("GET", "v1/withdraws/chance", query)(client)
 end
 
-function withdraw_info(client::Client; kw...)
+function withdraw_info(client::BithumbClient; kw...)
     return withdraw_info(client, WithdrawInfoQuery(; kw...))
 end
 
-end
+end 
