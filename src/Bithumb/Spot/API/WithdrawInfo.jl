5b5b3acb0f488bb813447d58682ddcd510c660e0
Evgenii Popov <epopov@bhft.com>
2025-01-31 17:38:39 +0300

    Add method WithdrawInfo for Bithumb

--- src/Bithumb/Spot/API/WithdrawInfo.jl
+++ src/Bithumb/Spot/API/WithdrawInfo.jl
@@ -1,12 +1,17 @@
 module WithdrawInfo
 
+export WithdrawInfoQuery,
+    WithdrawInfoData,
+    withdraw_info
+ 
+using Serde
+using Dates, NanoDates, TimeZones
+
 using CryptoExchangeAPIs.Bithumb
 using CryptoExchangeAPIs.Bithumb: Data
 using CryptoExchangeAPIs: Maybe, APIsRequest
 
-export WithdrawInfoQuery,
- WithdrawInfoData,
- withdraw_info
+
 
 Base.@kwdef mutable struct WithdrawInfoQuery <: BithumbPrivateQuery
     currency::String
@@ -15,49 +20,49 @@ Base.@kwdef mutable struct WithdrawInfoQuery <: BithumbPrivateQuery
 end
 
 struct MemberLevel <: BithumbData
-    security_level::Maybe{Int64}
-    fee_level::Maybe{Int64}
-    email_verified::Maybe{Bool}
-    identity_auth_verified::Maybe{Bool}
-    bank_account_verified::Maybe{Bool}
-    two_factor_auth_verified::Maybe{Bool}
-    locked::Maybe{Bool}
-    wallet_locked::Maybe{Bool}
+  security_level::Maybe{Int64}
+  fee_level::Maybe{Int64}
+  email_verified::Maybe{Bool}
+  identity_auth_verified::Maybe{Bool}
+  bank_account_verified::Maybe{Bool}
+  two_factor_auth_verified::Maybe{Bool}
+  locked::Maybe{Bool}
+  wallet_locked::Maybe{Bool}
 end
 
 struct Currency <: BithumbData
-    code::String
-    withdraw_fee::Float64
-    is_coin::Bool
-    wallet_state::String
-    wallet_support::Vector{String}
+  code::String
+  withdraw_fee::Float64
+  is_coin::Bool
+  wallet_state::String
+  wallet_support::Vector{String}
 end
 
 struct Account <: BithumbData
-    currency::String
-    balance::Float64
-    locked::Float64
-    avg_buy_price::Float64
-    avg_buy_price_modified::Bool
-    unit_currency::String
+  currency::String
+  balance::Float64
+  locked::Float64
+  avg_buy_price::Float64
+  avg_buy_price_modified::Bool
+  unit_currency::String
 end
 
 struct WithdrawLimit <: BithumbData
-    currency::String
-    minimum::Float64
-    onetime::Float64
-    daily::Float64
-    remaining_daily::Float64
-    fixed::Int64
-    can_withdraw::Bool
-    remaining_daily_krw::Maybe{Float64}
+  currency::String
+  minimum::Float64
+  onetime::Float64
+  daily::Float64
+  remaining_daily::Float64
+  fixed::Int64
+  can_withdraw::Bool
+  remaining_daily_krw::Maybe{Float64}
 end
 
 struct WithdrawInfoData <: BithumbData
-    member_level::MemberLevel
-    currency::Currency
-    account::Account
-    withdraw_limit::WithdrawLimit
+  member_level::MemberLevel
+  currency::Currency
+  account::Account
+  withdraw_limit::WithdrawLimit
 end
 """
     withdraw_info(client::BithumbClient, query::WithdrawInfoQuery)
@@ -82,14 +87,14 @@ using CryptoExchangeAPIs.Bithumb
 
 # Ensure that the environment variables BITHUMB_PUBLIC_KEY and BITHUMB_SECRET_KEY are set.
 bithumb_client = Bithumb.Client(;
-    base_url = "https://api.bithumb.com",
-    public_key = ENV["BITHUMB_PUBLIC_KEY"],
-    secret_key = ENV["BITHUMB_SECRET_KEY"],
+  base_url = "https://api.bithumb.com",
+  public_key = ENV["BITHUMB_PUBLIC_KEY"],
+  secret_key = ENV["BITHUMB_SECRET_KEY"],
 )
 result = WithdrawInfo.withdraw_info(
-    bithumb_client;
-    currency = "BTC",
-    net_type = "BTC",
+  bithumb_client;
+  currency = "BTC",
+  net_type = "BTC",
 )
 to_pretty_json(result.result)
 ```
@@ -101,39 +106,39 @@ to_pretty_json(result.result)
   "date": null,
   "data": {
     "member_level": {
-      "security_level": 3,
-      "fee_level": 1,
-      "email_verified": true,
-      "identity_auth_verified": true,
-      "bank_account_verified": true,
-      "two_factor_auth_verified": true,
-      "locked": false,
-      "wallet_locked": false
+    "security_level": 3,
+    "fee_level": 1,
+    "email_verified": true,
+    "identity_auth_verified": true,
+    "bank_account_verified": true,
+    "two_factor_auth_verified": true,
+    "locked": false,
+    "wallet_locked": false
     },
     "currency": {
-      "code": "BTC",
-      "withdraw_fee": 0.0005,
-      "is_coin": true,
-      "wallet_state": "working",
-      "wallet_support": ["KRW", "BTC"]
+    "code": "BTC",
+    "withdraw_fee": 0.0005,
+    "is_coin": true,
+    "wallet_state": "working",
+    "wallet_support": ["KRW", "BTC"]
     },
     "account": {
-      "currency": "BTC",
-      "balance": 1.0,
-      "locked": 0.0,
-      "avg_buy_price": 10000.0,
-      "avg_buy_price_modified": false,
-      "unit_currency": "KRW"
+    "currency": "BTC",
+    "balance": 1.0,
+    "locked": 0.0,
+    "avg_buy_price": 10000.0,
+    "avg_buy_price_modified": false,
+     "unit_currency": "KRW"
     },
     "withdraw_limit": {
-      "currency": "BTC",
-      "minimum": 0.001,
-      "onetime": 2.0,
-      "daily": 5.0,
-      "remaining_daily": 5.0,
-      "fixed": 0,
-      "can_withdraw": true,
-      "remaining_daily_krw": null
+    "currency": "BTC",
+    "minimum": 0.001,
+    "onetime": 2.0,
+    "daily": 5.0,
+    "remaining_daily": 5.0,
+    "fixed": 0,
+    "can_withdraw": true,
+    "remaining_daily_krw": null
     }
   }
 }
@@ -141,12 +146,11 @@ to_pretty_json(result.result)
 """
 function withdraw_info(client::BithumbClient, query::WithdrawInfoQuery)
   CryptoExchangeAPIs.request_sign!(client, query, "v1/withdraws/chance")
-  
   return APIsRequest{Data{WithdrawInfoData}}("GET", "v1/withdraws/chance", query)(client)
-  end
+end
   
-  function withdraw_info(client::BithumbClient; kw...)
+function withdraw_info(client::BithumbClient; kw...)
   return withdraw_info(client, WithdrawInfoQuery(; kw...))
-  end
+end
   
-  end
+end
